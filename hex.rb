670046ce9d4bf7ce5d1e96f8a1bba69fe10f36bf
Alex N'Guessan <alexmarcaureln@gmail.com>
2019-04-26 23:07:46 +0000

    add convert from base 10 to another one

--- hex.rb
+++ hex.rb
@@ -6,10 +6,20 @@ def to_decimal(base, num)
     return result
 end
 
+def to_base_x(base, num)
+    result = Array.new
+    begin
+        result.push(num%base)
+        num /= base
+    end while num/base!=0
+    result.push(num%base) # the loop end when the divsion answer is 0 so it doesn't calculate the last modulo
+    return result.reverse.join
+end
+
 puts "********** Hex converter **********"
-puts "Convertion base-x vers base-10"
+puts "Convertion base-10 vers base-x"
 print "Veillez choisir votre base de numération : "
 base = gets.to_i
 print "Veillez saisir le nombre à convertir : "
 num = gets.to_i
-print "Résultat : #{to_decimal(base, num)}"
+print "Résultat : #{to_base_x(base, num)}"
