64d713f9430b29e48725baedd7e0c40236c16a8b
Alex N'Guessan <alexmarcaureln@gmail.com>
2019-04-26 22:52:59 +0000

    Refactoring and set a func for conversion to decimal

--- hex.rb
+++ hex.rb
@@ -1,13 +1,15 @@
+def to_decimal(base, num)
+    result = 0
+    for i in 0..num.digits.length-1 do
+        result += num.digits[i]*(base**i)
+    end
+    return result
+end
+
 puts "********** Hex converter **********"
 puts "Convertion base-x vers base-10"
 print "Veillez choisir votre base de numération : "
 base = gets.to_i
 print "Veillez saisir le nombre à convertir : "
 num = gets.to_i
-tab = num.digits
-res = 0
-for i in 0..tab.length-1 do
-    tab[i] = tab[i]*(base**i)
-    res += tab[i]
-end
-print "Résultat : #{res}"
+print "Résultat : #{to_decimal(base, num)}"
